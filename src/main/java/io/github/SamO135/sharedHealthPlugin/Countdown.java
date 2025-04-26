f656674fe6d1cde65eceea5f92884f47edcef1f2
Samuel Ogg <56794292+SamO135@users.noreply.github.com>
2025-04-26 02:36:49 +0100

    add world border during countdown

--- src/main/java/io/github/SamO135/sharedHealthPlugin/Countdown.java
+++ src/main/java/io/github/SamO135/sharedHealthPlugin/Countdown.java
@@ -3,6 +3,8 @@ package io.github.SamO135.sharedHealthPlugin;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.title.Title;
 import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.WorldBorder;
 import org.bukkit.scheduler.BukkitTask;
 
 import java.util.function.Consumer;
@@ -23,12 +25,26 @@ public class Countdown implements Consumer<BukkitTask> {
             countdownText = "GO";
         }
 
+        // create title
         Component mainTitle = Component.text(countdownText);
         Component subtitle = Component.text("");
-
         Title title = Title.title(mainTitle, subtitle);
-
         Bukkit.getServer().showTitle(title);
+
+        // create world border
+        World world = Bukkit.getWorld("world_run");
+        if (world != null) {
+            WorldBorder border = world.getWorldBorder();
+            border.setCenter(world.getSpawnLocation());
+            border.setSize(10);
+
+            if (remainingTime <= 0){
+                //increase world border with an animation
+                int borderAnimationDuration = 5;
+                border.setSize(500, borderAnimationDuration);
+                Bukkit.getScheduler().runTaskLater(plugin, border::reset, borderAnimationDuration * 20L);
+            }
+        }
 
         remainingTime--;
         if (remainingTime < 0) {
